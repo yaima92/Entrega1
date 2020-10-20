70db90bc008432b03ff56e7bd23bff8f33a06afc
Yaima <yaimap1992@gmail.com>
2020-10-20 15:34:01 -0300

    cart validation

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,9 @@
+var cartList=[]; // Ponemos la lista como global, así la podemos usar en todo
+//el script
+var envio=0; //envio es para calcular el porcentaje
+
+
+
 function showArticlesList(cartList){
 
     let htmlContentToAppend = "";
@@ -11,56 +17,123 @@ function showArticlesList(cartList){
              </td>
              <td stcope="col"><strong> `+ cartArticles.name +`</strong></td>
              <td scope="col" id="productCostInput">`+ cartArticles.currency +``+ cartArticles.unitCost +`</td>
-             <td scope="col"><input type="number" class="form-control" onchange="calcular(${i}, ${cartArticles.unitCost})" id="productCountInput${i}" placeholder="" required="" value="0" min="0"></th>
-             <td scope="col"><strong id="subtotal${i}" onchange="calcularSubtotal(${i})"</strong></th>
-             <td scope="col"><button id="boton-vaciar" type="button" class="borrar-produto btn btn-danger" data-id="${cartArticles.id}">X</button></td>
+             <td scope="col"><input type="number" class="form-control" onchange="calcular()" id="productCountInput${i}" placeholder="" required="" value=${cartArticles.count} min="0"></th>
+             <td scope="col"><strong id="subtotal${i}"></strong></td>
+             <td scope="col"><button id="boton-vaciar" type="button" class="borrar-produto btn btn-danger" onclick="articles(${i})" name="${cartArticles.name}">X</button></td>
              </tr>
             `            
         }
         document.getElementById("articulos").innerHTML = htmlContentToAppend;
+        calcular();
     };
-
-    function calcular(i, costoUnitario){
-        var cantidad = document.getElementById('productCountInput' + i).value; 
-        var total = costoUnitario * cantidad;
-        document.getElementById('subtotal'+i).innerHTML=total; 
-    }
-    
-    
-    
     
     let productCost = 0;
     let productCount = 0;
-    let comissionPercentage = 15;
+    let comissionPercentage = 0.15;
     let MONEY_SYMBOL = "$";
     let DOLLAR_CURRENCY = "Dólares (USD)";
     let PESO_CURRENCY = "Pesos Uruguayos (UYU)";
     let DOLLAR_SYMBOL = "USD ";
     let PESO_SYMBOL = "UYU ";
-    let PERCENTAGE_SYMBOL = '%';
-    let SUCCESS_MSG = "¡Se ha realizado la publicación con éxito! :)";
-    let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 
-    function calcular(i, costoUnitario){
-        var cantidad = document.getElementById('productCountInput' + i).value; 
-        var total = costoUnitario * cantidad;
-        document.getElementById('subtotal'+i).innerHTML=total; 
-        for(let i = 0; i < cartList.articles.length; i++){
-            document.getElementById('productCostText').innerHTML=total + total;
+    function calcular(){
+        var totallinea=0; //resultado de costo unitario * cantidad;
+        var subtotal = 0; //resultado de sumar totallinea
+        var total=0; //resultado de sumar subtotales;
+        for( i = 0; i < cartList.articles.length; i++){
+            var cantidad=parseInt(document.getElementById('productCountInput' + i).value); //obtengo la cantidad
+            if(cartList.articles[i].currency == "UYU"){ 
+            totallinea= parseFloat((cartList.articles[i].unitCost)*cantidad/ 40);//calculo el precio
+            } else if(cartList.articles[i].currency == "USD"){
+                totallinea= parseFloat(cartList.articles[i].unitCost)*cantidad;
+            }
+            document.getElementById('subtotal'+i).innerHTML= totallinea + "USD";//escribo
+            subtotal+=totallinea; //sumo al subtotal
         }
+        envio=subtotal * comissionPercentage;
+        total=subtotal + envio;
+        document.getElementById('envio').innerHTML= MONEY_SYMBOL + envio.toFixed(2);   
+        document.getElementById('subtotal').innerHTML= MONEY_SYMBOL + subtotal.toFixed(2);
+        document.getElementById('total').innerHTML= MONEY_SYMBOL + total.toFixed(2); 
+    }
 
-    
+    //Función borrar 
+    function articles(i){ 
+        cartList.articles.splice(i, 1);
+        showArticlesList(cartList);
+   calcular();
+    }
 
+    const cliente = document.getElementById('cliente');
+    const correo = document.getElementById('correo');
+    const pago = document.getElementById('pago')
 
-    let unitCostToShow = MONEY_SYMBOL + total.toFixed(2);
-    let comissionToShow = MONEY_SYMBOL + (Math.round(( total * 15)/ 100).toFixed(2));
-    let totalCostToShow = MONEY_SYMBOL + Math.round((total + (total * 15)/ 100)).toFixed(2);
+    function procesarCompra(e){
+        e.preventDefault();
 
-    unitProductCostHTML.innerHTML = unitCostToShow;
-    comissionCostHTML.innerHTML = comissionToShow;
-    totalCostHTML.innerHTML = totalCostToShow;
+        if(cartList.articles.length === 0){
+            Swal.fire({ 
+                icon: 'error',
+                title: 'Oops...',
+                text: 'No hay productos, seleccione alguno',
+                timer: 2000,
+                showConfirmButton: false
+            }).then(function(){
+                window.location = 'cart.html';
+            })
+        }
+        else if(cliente.value === '' || correo.value === ''){
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Complete todos los campos requeridos',
+                timer: 2000,
+                showConfirmButton: false 
+        })
+        correo.classList.add('is-invalid');
+          correo.classList.remove('is-valid');
+          cliente.classList.add('is-invalid');
+          cliente.classList.remove('is-valid');
+    }else{
+
+        correo.classList.remove('is-invalid');
+          correo.classList.add('is-valid');
+          cliente.classList.remove('is-invalid');
+         cliente.classList.add('is-valid');
+
+        const cargandoGif = document.querySelector('#cargando');
+        cargandoGif.style.display = 'block';
+
+        const enviado = document.createElement('img');
+        enviado.src = 'img/mail.gif';
+        enviado.style.display = 'block';
+        enviado.width = '150';
+
+        setTimeout(()=>{
+            cargandoGif.style.display = 'none';
+            document.querySelector('#loaders').appendChild(enviado);
+            setTimeout(()=>{
+                  enviado.remove();
+                window.location = "cover.html";
+            },1500);
+        }, 3000);
+    }
 }
 
+/*const botonComprobar = document.getElementsByClassName('btn btn-success btn-block')
+        botonComprobar.addEventListener('click', validar);
+      
+      function validar() {
+        const correo = document.getElementById('correo');
+
+        if(correo.value === ""){
+          correo.classList.add('is-invalid');
+          correo.classList.remove('is-valid');
+        }else{
+          correo.classList.remove('is-invalid');
+          correo.classList.add('is-valid');
+        }
+      }*/
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -69,10 +142,11 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CART_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
             cartList = resultObj.data;
-            //Muestro los comentarios ordenados
+            //Muestro la lista de articulos.
             showArticlesList(cartList);
     }
 
+    //Calculo la comisión de envío.
     document.getElementById("premium").addEventListener("change", function(){
         comissionPercentage = 0.15;
         calcular();
@@ -88,6 +162,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         calcular();
     });
 
+    //Cambio según tipo de monedas.
     document.getElementById("productCurrency").addEventListener("change", function(){
         if (this.value == DOLLAR_CURRENCY)
         {
@@ -101,6 +176,11 @@ document.addEventListener("DOMContentLoaded", function(e){
         calcular();
     });
 
-});
-});
+    document.getElementById("boton-vaciar").addEventListener("click", function(){
+        
+        calcular();
+    });
 
+    document.getElementById('procesar-compra').addEventListener('click', procesarCompra);
+});
+});
